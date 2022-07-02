d2253c3f7fc59ac5e772be74d838ffb4694e9c7d
Mitch <mitchell.bounds88@gmail.com>
2022-07-02 15:30:42 -0500

    Initial website

---
+++ index.js
@@ -0,0 +1,23 @@
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+var request = new XMLHttpRequest()
+request.open('GET', 'https://vinkd0g0wj.execute-api.us-east-1.amazonaws.com/dev/add-visit', true)
+request.send()
+
+request = new XMLHttpRequest()
+request.open('GET', 'https://vinkd0g0wj.execute-api.us-east-1.amazonaws.com/dev/get-count', true)
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  if (request.status >= 200 && request.status < 400) {
+   counterContainer.innerHTML = data['body'];
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = 'Failed to query database'
+    app.appendChild(errorMessage)
+  }
+}
+
+request.send()
