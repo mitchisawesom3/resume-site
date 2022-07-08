e7468d18004917c9145cbca28a977b5b06b96881
Mitch <mitchell.bounds88@gmail.com>
2022-07-07 23:00:31 -0500

    Update API URL

--- public/index.js
+++ public/index.js
@@ -3,11 +3,11 @@ var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://vinkd0g0wj.execute-api.us-east-1.amazonaws.com/dev/add-visit', true)
+request.open('GET', 'https://ml6aul2rz8.execute-api.us-east-1.amazonaws.com/dev/add-visit', true)
 request.send()
 
 request = new XMLHttpRequest()
-request.open('GET', 'https://vinkd0g0wj.execute-api.us-east-1.amazonaws.com/dev/get-count', true)
+request.open('GET', 'https://ml6aul2rz8.execute-api.us-east-1.amazonaws.com/dev/get-count', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
